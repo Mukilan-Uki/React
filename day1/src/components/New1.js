3e94c869afcfb5f080ea3767896efe1e70bffc5d
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-10-08 14:28:07 +0530

    day 1

---
+++ day1/src/components/New1.js
@@ -0,0 +1,13 @@
+import './New.css';
+
+export default function New1(){
+    return(
+        <>
+        <h1>About Me!</h1>
+        <h3>My name is mukilan.</h3>
+        <h3>Iam 20 years old.</h3>
+        <h3>My friends are so dumb.</h3>
+        <input type="text" class="form-control" id="name" placeholder="Enter Your Name"></input>
+        </>
+    )
+}
---
+++ day1/src/components/New2.js
@@ -0,0 +1,12 @@
+
+export default function New2(){
+    return(
+        <>
+        <h1>About Freind!</h1>
+        <h3>My name is friend.</h3>
+        <h3>Iam 20 years old.</h3>
+        <h3>My friends are so dumb.</h3>
+        <button>hi bro</button>
+        </>
+    )
+}
