0196bfdbbbdf1186b8b4028dbb08001ca3fb8436
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-10-10 15:58:47 +0530

    day 2 react

---
+++ day2/src/components/Day2.jsx
@@ -0,0 +1,7 @@
+export default function Day2(){
+    return(
+        <>
+        <h1 style={{color:'blue',margin:'50px',display:'flex',justifyContent:'center'}}>Hellooo!</h1>
+        </>
+    )
+}
