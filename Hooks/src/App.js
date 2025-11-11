a1dfe69a144e92f8ac602be67c78a019c0196169
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-11-11 15:05:47 +0530

    shop web page

--- Hooks/src/App.js
+++ Hooks/src/App.js
@@ -1,16 +1,9 @@
-import UseRef from './components/day11/UseRef';
-import UseReducer from './components/day11/UseReducer';
-import UseMemo from './components/day11/UseMemo';
-import { UseContext1, UseContext2 } from './components/day11/UseContext';
+import ProductManagement from './components/Exercise/ProductManagement';
 
 function App() {
   return (
     <>
-      <UseRef></UseRef>
-      <UseReducer></UseReducer>
-      {/* <UseMemo></UseMemo> */}
-      <UseContext1></UseContext1>
-      <UseContext2></UseContext2>
+      <ProductManagement></ProductManagement>
     </>
   );
 }
