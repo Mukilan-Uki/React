284a078164bce5edea79dcda6ced008789b59964
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-12-01 15:46:55 +0530

    tailwind

---
+++ tailwind/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (<>
+    <h1 className="text-5xl font-bold [mask-image:linear-gradient(to_top,white,transparent)]">hiii</h1>
+    <button class="hover:focus:bg-purple-500">asdfgkl</button>
+    <div class="container-query">
+  <div class="@lg:text-4xl @md:text-xl">
+    Responsive inside container
+  </div>
+</div>
+    </>
+  );
+}
+
+export default App;
