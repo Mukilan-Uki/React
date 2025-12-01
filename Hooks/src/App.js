fff027d7450d78b0a7cf8e6a81ff5584b98170a0
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-12-01 15:48:44 +0530

    hook

--- Hooks/src/App.js
+++ Hooks/src/App.js
@@ -1,10 +1,11 @@
 import Task from './components/Exercise/day13/Task';
 import '../../Hooks/src/components/Exercise/day13/styles.css'
+import Data from './components/Assignment/Data';
 
 function App() {
   return (
     <>
-      <Task></Task>
+      <Data></Data>
     </>
   );
 }
